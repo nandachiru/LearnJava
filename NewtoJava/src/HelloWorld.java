5cb0cb0f013c57dbd2cd3f2853aac9378e7f67c8
Nanda <nandachiru@gmail.com>
2025-05-27 13:19:02 -0400

    Commiting code

---
+++ NewtoJava/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("I just coded my first java program");
+        System.out.println("My Name: Chidanaanda Madala");
+        System.out.println("Java Goal: Be proficient in Java");
+        System.out.println("Purpose:\tLearning Java and be proficient in Java");
+        System.out.println("Target: \n10 Days");
+        System.out.print("Hello ");
+        System.out.print("world");
+        System.out.print("!");
+
+    }
+}
